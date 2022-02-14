59f86d6f96a96964897e86d01a3da25cd1234269
ahDevGit <justalihaider@gmail.com>
2022-02-14 15:47:22 +0600

    Verify Pin function Added using onclick() method

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ document.getElementById('keypad').addEventListener('click', function (event) {
   const number = event.target.innerText;
   const calcInput = document.getElementById('typed-numbers');
   if (isNaN(number)) {
-    console.log(number);
+    // console.log(number);
     if (number == 'C') {
       calcInput.value = '';
     }
@@ -30,3 +30,22 @@ document.getElementById('keypad').addEventListener('click', function (event) {
     calcInput.value = newNumber;
   }
 })
+
+function verifyPin() {
+  // console.log('clicked');
+  const generatedPin = document.getElementById('pin-input').value;
+  const verifyPin = document.getElementById('typed-numbers').value;
+
+  const pinSuccess = document.getElementById('success');
+  const pinFailed = document.getElementById('fail');
+  if (generatedPin == verifyPin) {
+
+    pinSuccess.style.display = 'block';
+    pinFailed.style.display = 'none';
+  }
+  else {
+
+    pinFailed.style.display = 'block';
+    pinSuccess.style.display = 'none';
+  }
+}
